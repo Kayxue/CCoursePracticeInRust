0fde213cdef53afb3aa39f6c7f88e24cec3b18c3
Kayxue <s0990006@gmail.com>
2022-10-18 18:02:09 +0800

    2022/10/18 finished Practice3-2

---
+++ Practice/Practice3-2/README.md
@@ -0,0 +1,20 @@
+# 第三次第二題
+## 題目
+Ask the user to give a non-negative integer and a base (2 ≤ base ≤ 10).
+Print the number in the specified base.
+## 範例
+```
+請輸入一個非負整數：453
+請輸入進位制：10
+10進位表示法：453
+```
+```
+請輸入一個非負整數：453
+請輸入進位制：2
+2進位表示法：111000101
+```
+```
+請輸入一個非負整數：453
+請輸入進位制：8
+8進位表示法：705
+```
---
+++ Practice/Practice3-2/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+use std::io::Write;
+
+fn main() {
+    let mut numStr = String::new();
+    let mut baseStr = String::new();
+    let mut maxBase = 1;
+    print!("請輸入一個非負整數：");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut numStr).unwrap();
+    print!("請輸入進位制：");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut baseStr).unwrap();
+    let mut num: i32 = numStr.trim().parse().unwrap();
+    let base: i32 = baseStr.trim().parse().unwrap();
+    maxBase = base.clone();
+    while maxBase * base <= num {
+        maxBase *= base;
+    }
+    print!("{}進位表示法：", base);
+    while num > 0 {
+        print!("{}", num / maxBase);
+        num -= (num / maxBase) as i32 * maxBase;
+        maxBase /= base;
+    }
+    print!("\n");
+}
