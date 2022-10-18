3c62e90a48812e2d05fafadf2a670ed37832d7e4
Kayxue <s0990006@gmail.com>
2022-10-18 23:07:37 +0800

    2022/10/18 finished Practice2-3

---
+++ Practice/Practice2-3/README.md
@@ -0,0 +1,25 @@
+# 第二次第三題
+## 題目
+Ask the user to specify the number of the days in the month and the day of the week on which the month begins:
+print out a calendar for this month.
+## 範例
+```
+請問這個月有多少天？31
+請問這個月第一天是星期幾？(0=星期天, 1=星期一, ...) 2
+ S  M  T  W  T  F  S
+       1  2  3  4  5
+ 6  7  8  9 10 11 12
+13 14 15 16 17 18 19
+20 21 22 23 24 25 26
+27 28 29 30 31
+```
+```
+請問這個月有多少天？30
+請問這個月第一天是星期幾？(0=星期天, 1=星期一, ...) 4
+ S  M  T  W  T  F  S
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30
+```
---
+++ Practice/Practice2-3/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::io::Write;
+
+fn main() {
+    let mut dayCountStr = String::new();
+    let mut firstDayStr = String::new();
+    print!("請問這個月有多少天？");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut dayCountStr).unwrap();
+    print!("請問這個月第一天是星期幾？(0=星期天, 1=星期一, ...) ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut firstDayStr).unwrap();
+    let dayCount: i8 = dayCountStr.trim().parse().unwrap();
+    let firstDay: i8 = firstDayStr.trim().parse().unwrap();
+    let mut nowPrint: i8 = 1;
+    let mut startPrint = false;
+    println!(" S  M  T  W  T  F  S");
+    while nowPrint <= dayCount {
+        for i in 0..=6 {
+            if nowPrint > dayCount {
+                break;
+            }
+            if !startPrint {
+                if i == firstDay {
+                    print!("{:>2} ", nowPrint);
+                    nowPrint += 1;
+                    startPrint = true;
+                } else {
+                    print!("   ");
+                }
+            } else {
+                print!("{:>2} ", nowPrint);
+                nowPrint += 1;
+            }
+        }
+        print!("\n");
+    }
+}
