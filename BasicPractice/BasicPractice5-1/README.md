879720c17c498fe247070d5452654d330610f654
Kayxue <s0990006@gmail.com>
2022-10-19 23:00:07 +0800

    2022/10/19 finished BasicPractice5-1

---
+++ BasicPractice/BasicPractice5-1/README.md
@@ -0,0 +1,57 @@
+# 第五次作業第一題
+
+## 題目
+Write a function to check if a given date is an invalid date (不合法日期), whose function prototype should be:
+
+* `int isInvalidDate(int year, int month, int day); // in C `or
+* `bool isInvalidDate(int year, int month, int day); // in C++`
+Given a date (year/month/day), this function will return
+* `true` or 1 if it is NOT a valid date;
+* `false` or 0 otherwise.
+When check a date in Feburary,
+you also need to know if the given year is a leap year (閏年),
+so that you can know that there are 28 or 29 days in Feburary.
+Note that the rules of leap years are:
+
+* 逢四則閏 (4's multiples are leap years)
+* 逢百不閏 (but 100's multiples are not leap years)
+* 逢四百又閏 (but 400's multiples are leap years)
+Write a function to decide if a given year is a leap, whose function prototype should be:
+
+* `int isLeapYear(int year); // in C` or
+* `bool isLeapYear(int year); // in C++`
+Given a year, this function will return
+* `true` or 1 if it is a leap year;
+* `false` or 0 otherwise.
+Use the following main() to test your function.
+```
+int main() {
+    int ti, repeatTimes, year, month, day;
+    // get repeatTimes here
+    for (ti = 0; ti < repeatTimes; ti++) {
+        printf("\nInput a date (year/month/day): ");
+        scanf("%d/%d/%d", &year, &month, &day);
+        printf("%d/%d/%d %s a valid date.\n", year, month, day, isInvalidDate(year, month, day) ? "is not" : "is");
+    }
+    return 0;
+}
+```
+## 範例
+```
+How many sets of test data: 5
+
+Input a date (year/month/day): 2015/-7/23
+2015/-7/23 is not a valid date.
+
+Input a date (year/month/day): 2015/5/0
+2015/5/0 is not a valid date.
+
+Input a date (year/month/day): 2016/2/29
+2016/2/29 is a valid date.
+
+Input a date (year/month/day): 2015/2/29
+2015/2/29 is not a valid date.
+
+Input a date (year/month/day): 777/3/6
+777/3/6 is a valid date.
+```
---
+++ BasicPractice/BasicPractice5-1/src/main.rs
@@ -0,0 +1,63 @@
+use std::io;
+use std::io::Write;
+
+const dayCount: [i8; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+fn isLeap(year: &i32) -> bool {
+    year % 400 == 0 || (year % 4 == 0 && !(year % 100 == 0))
+}
+
+fn isInvalidDate(year: &i32, month: &i32, day: &i32) -> bool {
+    if *year <= 0 || *month <= 0 || *month > 12 || *day <= 0 {
+        return true;
+    }
+    if *month == 2 {
+        if isLeap(year) {
+            if *day <= 29 {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            if *day <= 28 {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    } else {
+        if *day <= dayCount[(*month - 1) as usize] as i32 {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+fn main() {
+    let mut repeatTimesStr = String::new();
+    print!("How many sets of test data: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut repeatTimesStr).unwrap();
+    let repeatTime: i32 = repeatTimesStr.trim().parse().unwrap();
+    for i in 0..repeatTime {
+        let mut dateStr = String::new();
+        print!("\nInput a date (year/month/day): ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut dateStr).unwrap();
+        let dateArr: Vec<i32> = dateStr
+            .trim()
+            .split("/")
+            .map(|e| e.parse().unwrap())
+            .collect();
+        let year = dateArr[0];
+        let month = dateArr[1];
+        let day = dateArr[2];
+        let result = isInvalidDate(&year, &month, &day);
+        if result {
+            println!("{} is not a valid date.", dateStr.trim());
+        } else {
+            println!("{} is a valid date.", dateStr.trim());
+        }
+    }
+}
