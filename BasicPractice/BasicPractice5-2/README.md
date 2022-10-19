9dbdd311aa54667b9abb9869bcbaa7576140ffe4
Kayxue <s0990006@gmail.com>
2022-10-19 23:04:56 +0800

    2022/10/19 finished BasicPractice5-2

---
+++ BasicPractice/BasicPractice5-2/README.md
@@ -0,0 +1,25 @@
+# 第五次作業第二題
+# 題目
+Get a positive integer n from the user.
+Print out the values of sin(x) and cos(x) functions where the angle x is 0⁄n * π, 1⁄n * π, 2⁄n * π, ⋯, 2n⁄n * π
+Present the angles after fraction reduction (約分).
+## 範例
+```
+n = 6
+x = 0 PI, sin(x) = 0.000000, cos(x) = 1.000000
+x = 1/6 PI, sin(x) = 0.500000, cos(x) = 0.866025
+x = 1/3 PI, sin(x) = 0.866025, cos(x) = 0.500000
+x = 1/2 PI, sin(x) = 1.000000, cos(x) = 0.000000
+x = 2/3 PI, sin(x) = 0.866025, cos(x) = -0.500000
+x = 5/6 PI, sin(x) = 0.500000, cos(x) = -0.866025
+x = 1 PI, sin(x) = 0.000000, cos(x) = -1.000000
+x = 7/6 PI, sin(x) = -0.500000, cos(x) = -0.866025
+x = 4/3 PI, sin(x) = -0.866025, cos(x) = -0.500000
+x = 3/2 PI, sin(x) = -1.000000, cos(x) = -0.000000
+x = 5/3 PI, sin(x) = -0.866025, cos(x) = 0.500000
+x = 11/6 PI, sin(x) = -0.500000, cos(x) = 0.866025
+x = 2 PI, sin(x) = -0.000000, cos(x) = 1.000000
+```
+
+## 備註
+PI=3.14159265358979323846
---
+++ BasicPractice/BasicPractice5-2/src/main.rs
@@ -0,0 +1,45 @@
+use std::io;
+use std::io::Write;
+
+const PI: f32 = 3.14159265358979323846;
+
+fn gcd(a1: &i32, b1: &i32) -> i32 {
+    let mut a = *a1;
+    let mut b = *b1;
+    let mut t: i32;
+    while b != 0 {
+        t = b.clone();
+        b = a % b;
+        a = t;
+    }
+    a
+}
+
+fn main() {
+    let mut nStr = String::new();
+    print!("n = ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut nStr).unwrap();
+    let n: i32 = nStr.trim().parse().unwrap();
+    for i in 0..=2 * n {
+        if i % n == 0 {
+            println!(
+                "x = {} PI, sin(x) = {:.6}, cos(x) = {:.6}",
+                i / n,
+                (i as f32 * PI/ n as f32).sin(),
+                (i as f32 * PI/ n as f32).cos()
+            );
+        } else {
+            let tgcd = gcd(&i, &n);
+            let s = (i / tgcd) as f32;
+            let t = (n / tgcd) as f32;
+            println!(
+                "x = {}/{} PI, sin(x) = {:.6}, cos(x) = {:.6}",
+                s,
+                t,
+                (s *PI/ t).sin(),
+                (s *PI/ t).cos()
+            );
+        }
+    }
+}
