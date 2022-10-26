f9bb2a3f7ddb58f68712ab5ca666fb793f0c79ab
Kayxue <s0990006@gmail.com>
2022-10-26 22:18:19 +0800

    2022/10/26 finished BasicPractice6-2

--- BasicPractice/BasicPractice6-2/src/main.rs
+++ BasicPractice/BasicPractice6-2/src/main.rs
@@ -4,7 +4,13 @@ use std::io::Write;
 fn main() {
     let mut inputStr = String::new();
     io::stdin().read_line(&mut inputStr).unwrap();
-    for  c in inputStr.trim().chars() {
-        println!("[{}] {} ==> [{}] {}",c,c as u8,(((c as u8)+1)as char),(c as u8)+1)
+    for c in inputStr.trim().chars() {
+        println!(
+            "[{}] {} ==> [{}] {}",
+            c,
+            c as u8,
+            (((c as u8) + 1) as char),
+            (c as u8) + 1
+        )
     }
 }
