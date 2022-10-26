8d75362e6f53b780b4ef98bad022457c2d048a37
Kayxue <s0990006@gmail.com>
2022-10-26 22:20:21 +0800

    2022/10/26 finished BasicPractice6-1

--- BasicPractice/BasicPractice6-1/src/main.rs
+++ BasicPractice/BasicPractice6-1/src/main.rs
@@ -1,16 +1,15 @@
-use core::num;
 use std::io;
 use std::io::Write;
 
 fn main() {
-    let mut dataCountStr=String::new();
+    let mut dataCountStr = String::new();
     print!("How many sets of test data: ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut dataCountStr).unwrap();
-    let dataCount:i32=dataCountStr.trim().parse().unwrap();
-    for i in 0..dataCount{
-        let mut numStr=String::new();
-        let mut intervalStr=String::new();
+    let dataCount: i32 = dataCountStr.trim().parse().unwrap();
+    for i in 0..dataCount {
+        let mut numStr = String::new();
+        let mut intervalStr = String::new();
         print!("\n");
         print!("Please input a large integer: ");
         io::stdout().flush().unwrap();
@@ -18,13 +17,13 @@ fn main() {
         print!("Please input an interval: ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut intervalStr).unwrap();
-        let mut num:i128=numStr.trim().parse().unwrap();
-        let interval:i32=intervalStr.trim().parse().unwrap();
-        println!("Your input is [{}]",num);
-        println!("Its next 10 numbers by adding {} are:",interval);
-        for i in 0..10{
+        let mut num: i128 = numStr.trim().parse().unwrap();
+        let interval: i32 = intervalStr.trim().parse().unwrap();
+        println!("Your input is [{}]", num);
+        println!("Its next 10 numbers by adding {} are:", interval);
+        for i in 0..10 {
             num += interval as i128;
-            println!("{}",num);
+            println!("{}", num);
         }
         println!("Over!!");
     }
