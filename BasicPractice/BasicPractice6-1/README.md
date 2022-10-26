c09d165aaba5764c8fd0e851e030f4e4dacb6ad3
Kayxue <s0990006@gmail.com>
2022-10-26 22:06:18 +0800

    2022/10/26 finished BasicPractice6-1

---
+++ BasicPractice/BasicPractice6-1/README.md
@@ -0,0 +1,58 @@
+# 第六次作業第一題
+## 題目
+Define an unsigned integer variable numU.
+Ask the user to key in a large integer (≥ 2,200,000,000) and an interval n.
+Your program will print out 10 of its successors in the interval n.
+Do it for several times.
+## 範例
+```
+How many sets of test data: 3
+
+Please input a large integer: 3214567890
+Please input an interval: 8
+Your input is [3214567890]
+Its next 10 numbers by adding 8 are:
+3214567898
+3214567906
+3214567914
+3214567922
+3214567930
+3214567938
+3214567946
+3214567954
+3214567962
+3214567970
+Over!!
+
+Please input a large integer: 4024549127
+Please input an interval: 13
+Your input is [4024549127]
+Its next 10 numbers by adding 13 are:
+4024549140
+4024549153
+4024549166
+4024549179
+4024549192
+4024549205
+4024549218
+4024549231
+4024549244
+4024549257
+Over!!
+
+Please input a large integer: 2984341603
+Please input an interval: -6
+Your input is [2984341603]
+Its next 10 numbers by adding -6 are:
+2984341597
+2984341591
+2984341585
+2984341579
+2984341573
+2984341567
+2984341561
+2984341555
+2984341549
+2984341543
+Over!!
+```
---
+++ BasicPractice/BasicPractice6-1/src/main.rs
@@ -0,0 +1,31 @@
+use core::num;
+use std::io;
+use std::io::Write;
+
+fn main() {
+    let mut dataCountStr=String::new();
+    print!("How many sets of test data: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut dataCountStr).unwrap();
+    let dataCount:i32=dataCountStr.trim().parse().unwrap();
+    for i in 0..dataCount{
+        let mut numStr=String::new();
+        let mut intervalStr=String::new();
+        print!("\n");
+        print!("Please input a large integer: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut numStr).unwrap();
+        print!("Please input an interval: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut intervalStr).unwrap();
+        let mut num:i128=numStr.trim().parse().unwrap();
+        let interval:i32=intervalStr.trim().parse().unwrap();
+        println!("Your input is [{}]",num);
+        println!("Its next 10 numbers by adding {} are:",interval);
+        for i in 0..10{
+            num += interval as i128;
+            println!("{}",num);
+        }
+        println!("Over!!");
+    }
+}
